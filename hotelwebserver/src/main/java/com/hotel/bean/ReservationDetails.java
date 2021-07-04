a7bb09ba520ff882ca12f5d28bec3252050715e0
Ziwei.Liu <ziwei.liu@smu.ca>
2021-07-04 19:34:59 -0300

    Fix bug

--- hotelwebserver/src/main/java/com/hotel/bean/ReservationDetails.java
+++ hotelwebserver/src/main/java/com/hotel/bean/ReservationDetails.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.JoinColumn;
 
 @Entity
 @Table(name="HotelReservation")
